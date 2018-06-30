a924b96e6615ec0246fe915785cbc2d1a46e1a9e
LGiki <qq547221432@gmail.com>
2018-06-30 09:25:55 +0800

    Use Captive Portal Server to check online status

--- Linux/ruijie_jmu.sh
+++ Linux/ruijie_jmu.sh
@@ -1,21 +1,32 @@
 #!/bin/bash
 
+#If received logout parameter, sned a logout request to eportal server
 if [ "$1" = "logout" ]; then
   logoutResult=`curl -s -A "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.91 Safari/537.36" http://210.34.130.210/eportal/InterFace.do?method=logout`
   echo $logoutResult
   exit 0
 fi
 
+#If received parameters is less than 3, print usage
 if [ "$#" -lt "3" ]; then
   echo "Usage: ./ruijie_jmu.sh interface username password"
-  echo "interface can be \"chinamobile\", \"chinanet\" and \"chinaunicom\". If this parameter do not set as these value, it will use campus network as default interface."
+  echo "interface can be \"chinamobile\", \"chinanet\" and \"chinaunicom\". If interface do not set as these value, it will use campus network as default interface."
   echo "Example: ./ruijie_jmu.sh chinanet 201620000000 123456"
   echo "if you want to logout, use: ./ruijie_jmu.sh logout"
   exit 1
 fi
 
-loginPageURL=`curl -s "http://139.199.182.194/Ruijie.html" | awk -F \' '{print $2}'`
-#Get Ruijie login page URL. If device already online, loginPageURL will be null. The jmuRuijie.html is a blank page.
+#Exit the script when is already online, use www.google.cn/generate_204 to check the online status
+captiveReturnCode=`curl -s -I -m 10 -o /dev/null -s -w %{http_code} http://www.google.cn/generate_204`
+if [ "$captiveReturnCode" = "204" ]; then
+  echo "You are already online!"
+  exit 0
+fi
+
+#If not online, begin Ruijie Auth
+
+#Get Ruijie login page URL
+loginPageURL=`curl -s "http://www.google.cn/generate_204" | awk -F \' '{print $2}'`
 
 chinamobile="%25E7%25A7%25BB%25E5%258A%25A8%25E5%25AE%25BD%25E5%25B8%25A6%25E6%258E%25A5%25E5%2585%25A5"
 chinanet="%25E7%2594%25B5%25E4%25BF%25A1%25E5%25AE%25BD%25E5%25B8%25A6%25E6%258E%25A5%25E5%2585%25A5"
@@ -44,12 +55,6 @@ if [ -z "$interface" ]; then
   interface="$campus"
 fi
 
-#Exit when already online
-if [ -z "$loginPageURL" ]; then
-  echo "You are already online!"
-  exit 0
-fi
-
 #Structure loginURL
 loginURL=`echo $loginPageURL | awk -F \? '{print $1}'`
 loginURL="${loginURL/index.jsp/InterFace.do?method=login}"
@@ -59,7 +64,7 @@ queryString=`echo $loginPageURL | awk -F \? '{print $2}'`
 queryString="${queryString//&/%2526}"
 queryString="${queryString//=/%253D}"
 
-#Login and output the result
+#Send Ruijie eportal auth request and output result
 if [ -n "$loginURL" ]; then
   authResult=`curl -s -A "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.91 Safari/537.36" -e "$loginPageURL" -b "EPORTAL_COOKIE_USERNAME=; EPORTAL_COOKIE_PASSWORD=; EPORTAL_COOKIE_SERVER=; EPORTAL_COOKIE_SERVER_NAME=; EPORTAL_AUTO_LAND=; EPORTAL_USER_GROUP=%E5%AD%A6%E7%94%9F%E5%8C%85%E6%9C%88; EPORTAL_COOKIE_OPERATORPWD=;" -d "userId=$2&password=$3&service=$interface&queryString=$queryString&operatorPwd=&operatorUserId=&validcode=&passwordEncrypt=false" -H "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8" -H "Content-Type: application/x-www-form-urlencoded; charset=UTF-8" "$loginURL"`
   echo $authResult
--- Openwrt & LEDE/ruijie_general.sh
+++ Openwrt & LEDE/ruijie_general.sh
@@ -1,19 +1,23 @@
 #!/bin/sh
 
+#If received parameters is less than 2, print usage
 if [ "$#" -lt "2" ]; then
   echo "Usage: ./ruijie_general.sh username password"
   echo "Example: ./ruijie_general.sh 201620000000 123456"
   exit 1
 fi
 
-loginPageURL=`curl -s "http://139.199.182.194/Ruijie.html" | awk -F \' '{print $2}'`
-#Get Ruijie login page URL. If device already online, loginPageURL will be null. The jmuRuijie.html is a blank page.
-
-#Exit when already online
-if [ -z "$loginPageURL" ]; then
+#Exit the script when is already online, use www.google.cn/generate_204 to check the online status
+captiveReturnCode=`curl -s -I -m 10 -o /dev/null -s -w %{http_code} http://www.google.cn/generate_204`
+if [ "$captiveReturnCode" = "204" ]; then
   echo "You are already online!"
   exit 0
 fi
+
+#If not online, begin Ruijie Auth
+
+#Get Ruijie login page URL
+loginPageURL=`curl -s "http://www.google.cn/generate_204" | awk -F \' '{print $2}'`
 
 #Structure loginURL
 loginURL=`echo $loginPageURL | awk -F \? '{print $1}'`
@@ -25,7 +29,7 @@ queryString=`echo $loginPageURL | awk -F \? '{print $2}'`
 queryString="${queryString//&/%2526}"
 queryString="${queryString//=/%253D}"
 
-#Login and output the result
+#Send Ruijie eportal auth request and output result
 if [ -n "$loginURL" ]; then
   authResult=`curl -s -A "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.91 Safari/537.36" -e "$loginPageURL" -b "EPORTAL_COOKIE_USERNAME=; EPORTAL_COOKIE_PASSWORD=; EPORTAL_COOKIE_SERVER=; EPORTAL_COOKIE_SERVER_NAME=; EPORTAL_AUTO_LAND=; EPORTAL_USER_GROUP=%E5%AD%A6%E7%94%9F%E5%8C%85%E6%9C%88; EPORTAL_COOKIE_OPERATORPWD=;" -d "userId=$1&password=$2&service=&queryString=$queryString&operatorPwd=&operatorUserId=&validcode=&passwordEncrypt=false" -H "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8" -H "Content-Type: application/x-www-form-urlencoded; charset=UTF-8" "$loginURL"`
   echo $authResult
